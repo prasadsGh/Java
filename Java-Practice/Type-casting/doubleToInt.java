de8f97015ad26b0798de6991cc6809c4ea116b78
prasad patil <prasadpatil2309@gmail.com>
2022-07-01 20:43:58 +0530

    type casting

---
+++ Java-Practice/Type-casting/doubleToInt.java
@@ -0,0 +1,16 @@
+
+
+public class doubleToInt {
+    public static void main(String[] args)
+    {
+        double a=10.09,b=100.987;
+        System.out.println("Before type casting: "+"a: "+a+" "+"b: "+b+" ");
+;         int data1= (int) a;
+        int data2= (int) b;
+        System.out.println("After type casting: "+"a: "+data1+" "+"b: "+data2+"\n ");
+        System.out.println("printing those statement again of a and b: \n");
+         System.out.println("Before values: "+"a: "+a+" "+"b: "+b+" ");
+        
+            }
+    
+}
---
+++ Java-Practice/Type-casting/inttoDouble.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+public class inttoDouble {
+    public static void main(String[] args)
+    {
+        
+        int num=19;
+         double data=20.00;
+        double sum=num+data;
+        System.out.println(sum+"\n");
+
+
+
+    }
+    
+}
