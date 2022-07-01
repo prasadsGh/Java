6b989682e474b227563d5b43a5777df7a0a4d7e5
prasad patil <prasadpatil2309@gmail.com>
2022-07-01 20:09:41 +0530

    arithmetic operator

---
+++ Java-Practice/Operators/arithmeticOperator.java
@@ -0,0 +1,17 @@
+
+
+public class arithmeticOperator {
+    public static void main(String[] arg){
+        int a=10,b=20;
+        int res=a+b;
+        System.out.println("Addition is "+res);
+        res=a-b;
+        System.out.println("Susbtraction is "+res);
+        res=a*b;
+         System.out.println("Multiplication is "+res);
+         res=(b/a);
+          System.out.println("Division is "+res);
+
+    }
+    
+}
