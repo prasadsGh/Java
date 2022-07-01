87340d7032f01eb97bc84804af04f39bfc4a76ae
prasad patil <prasadpatil2309@gmail.com>
2022-07-01 21:09:00 +0530

    for loop

--- Java-Practice/loops/for_loop.java
+++ Java-Practice/loops/for_loop.java
@@ -14,7 +14,21 @@ public class for_loop {
             System.out.println((data*i)+ " ");
         }
     }
-
-
-    
+ 
 }
+// output:
+// Enter the number and we will print the table uptp 10: 
+// 2
+// and the table is: 
+// 2 
+// 4 
+// 6 
+// 8 
+// 10 
+// 12 
+// 14 
+// 16 
+// 18 
+// 20
+//same goes for the while and do while loop 
+//and also on thing continue and break have the same function in java as it has in the cpp
