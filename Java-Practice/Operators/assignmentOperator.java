bc2b8ee92386f0d5b2ead68121743a41b32fe506
prasad patil <prasadpatil2309@gmail.com>
2022-07-01 20:05:33 +0530

    operators

---
+++ Java-Practice/Operators/assignmentOperator.java
@@ -0,0 +1,13 @@
+// package Operators;
+
+public class assignmentOperator {
+    public static void main(String[] args)
+    {
+        double number=12.98675645;
+        System.out.println("The number is "+number );
+    }
+}
+//we can assignm one variable to another also
+
+// output:
+// The number is 12.98675645
