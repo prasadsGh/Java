21e83925b0e264065489c524afd043630f3a7845
prasad patil <prasadpatil2309@gmail.com>
2022-07-01 20:50:35 +0530

    comparison operator

---
+++ Java-Practice/Operators/comparsionOperator.java
@@ -0,0 +1,24 @@
+
+
+public class comparsionOperator {
+    public static void main(String[] args)
+    {
+        int a=10;
+        System.out.println(a<11);
+        System.out.println(a>100);
+        System.out.println(a>=10);
+        System.out.println(a==4);
+        System.out.println(a!=4);
+        boolean result=(a<=10);
+        System.out.println(result);
+    }
+
+    
+}
+// output:
+// true
+// false
+// true
+// false
+// true
+// true
