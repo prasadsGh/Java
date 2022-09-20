c0b51649597c6440e22ffe409f82fe761318631b
pict <pict@localhost.localdomain>
2022-09-20 12:08:44 +0530

    basic code

---
+++ Java-Practice/Basic/HelloWorld.java
@@ -0,0 +1,18 @@
+class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+        int mynum=100000000;
+        System.out.println(mynum);
+        String name="patil";
+        System.out.println(("prasad "+name ));
+        String full_name="prasad "+name;
+        System.out.println(full_name);
+
+        System.out.println("YOU SHOULD WATCH THIS--------------- ");
+        System.out.println("x=y=z=10 is allowed in java");
+        int x,y,z;
+        x=y=z=10;
+        System.out.println(x+" "+y+" "+z);
+
+    }
+}
