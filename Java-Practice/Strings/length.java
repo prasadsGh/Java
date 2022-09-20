ce96eed39a8d769ac27fa1eac3220a8c960b1726
pict <pict@localhost.localdomain>
2022-09-20 12:35:14 +0530

    string code

---
+++ Java-Practice/Strings/length.java
@@ -0,0 +1,11 @@
+// import java.util.*;
+public class length
+{
+    public static void main(String[] args) {
+        
+    String a="prasadpatil";
+    long b=a.length();
+    System.out.println("length of the string is "+ b);
+    
+}
+}
---
+++ Java-Practice/Strings/touppercase.java
@@ -0,0 +1,10 @@
+
+public class touppercase {
+    public static void main(String[] args) {
+        String a="Hello World";
+        System.out.println(a.toUpperCase());
+        System.out.println(a.toLowerCase());
+
+    }
+    
+}
